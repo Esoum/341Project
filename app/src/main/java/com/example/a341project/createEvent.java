4e3044b4827df4fe3d9578b88143bc01019fc96e
rryanh <ryanhuga@gmail.com>
2020-03-27 10:27:33 -0700

    events added

---
+++ app/src/main/java/com/example/a341project/createEvent.java
@@ -0,0 +1,62 @@
+package com.example.a341project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class createEvent extends AppCompatActivity {
+    private static int RESULT_LOAD_IMAGE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+        Button buttonLoadImage = findViewById(R.id.addphotobtn2);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+        }
+    }
+    public void onClickCreatedEvent(View view){
+        EditText editText12 = findViewById(R.id.editText12);
+        EditText editText16 = findViewById(R.id.editText16);
+        EditText editText17 = findViewById(R.id.editText17);
+        if (editText12.length() > 0 && editText16.length() > 0 && editText17.length() > 0){
+            Toast.makeText(this, "Your Event Has Been Created!", Toast.LENGTH_LONG).show();
+            finish();
+        }else{
+            Toast.makeText(this, "Missing Event Data! Fill in all Entries!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
