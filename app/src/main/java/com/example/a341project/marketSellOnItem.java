73c65509c2d8d292f01915ef89b872b1107b631f
rryanh <ryanhuga@gmail.com>
2020-04-03 13:30:33 -0700

    fixed

--- app/src/main/java/com/example/a341project/marketSellOnItem.java
+++ app/src/main/java/com/example/a341project/marketSellOnItem.java
@@ -47,7 +47,7 @@ public class marketSellOnItem extends AppCompatActivity {
             cursor.close();
         }
     }
-    public void onClickMarketSellOnItem(View view){
+    public void onClickCreatedEvent(View view){
         EditText editText12 = findViewById(R.id.editTextM12);
         EditText editText16 = findViewById(R.id.editTextM16);
         if (editText12.length() > 0 && editText16.length() > 0 ){
