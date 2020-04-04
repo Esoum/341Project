7ea1b9730abd450b73bb88429c7f3d363dfc9900
Michael Crouse <michaeljcrouse@gmail.com>
2020-04-03 17:39:42 -0700

    update login

--- app/src/main/java/com/example/a341project/eventMain.java
+++ app/src/main/java/com/example/a341project/eventMain.java
@@ -5,19 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class eventMain extends AppCompatActivity {
+
+    Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_main);
+
+        backBtn=findViewById(R.id.button7);
+
+        backBtn.setOnClickListener(new AdapterView.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                sendHome();
+            }
+        });
     }
 
     public void onClickCreateEvent(View view){
         Intent intent = new Intent(this, createEvent.class);
         startActivity(intent);
+    }
+
+    public void sendHome(){
+        Intent new_intent= new Intent(this, MainActivity.class);
+        startActivity(new_intent);
     }
 
 }
